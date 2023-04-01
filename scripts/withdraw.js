466f5f1cd369a406d1b17503c5045802ca10227b
Anshu Joshi <anshu.aj.joshi@gmail.com>
2023-04-01 22:35:51 +0530

    Add scripts

--- scripts/withdraw.js
+++ scripts/withdraw.js
@@ -1,15 +1,13 @@
+const { ethers, getNamedAccounts } = require("hardhat")
 
-const { ethers, getNamedAccounts } = require("hardhat");
 async function main() {
   const { deployer } = await getNamedAccounts()
   const fundMe = await ethers.getContract("FundMe", deployer)
   console.log(`Got contract FundMe at ${fundMe.address}`)
-  console.log("Funding contract...")
-  const transactionResponse = await fundMe.fund({
-    value: ethers.utils.parseEther("0.1"),
-  })
+  console.log("Withdrawing from contract...")
+  const transactionResponse = await fundMe.withdraw()
   await transactionResponse.wait()
-  console.log("Funded!")
+  console.log("Got it back!")
 }
 
 main()
